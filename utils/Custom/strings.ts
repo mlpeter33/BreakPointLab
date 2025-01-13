e6594307e55c2462bd081e39aaf08c496ff81054
MarianoCSC <123034983+mlpeter33@users.noreply.github.com>
2025-01-13 14:00:23 -0600

    Add string cases

--- utils/Custom/strings.ts
+++ utils/Custom/strings.ts
@@ -7,3 +7,18 @@ console.log(randomString);
 //uppercase alphanumeric string.
 const randomCapsString = generateRandomString(10, {lowercase: false, numbers: false });
 console.log(randomCapsString);
+
+//lowercase alphanumeric string.
+const randomLowerString = generateRandomString(10, {uppercase: false, numbers: false });
+console.log(randomLowerString);
+
+//numeric string.
+const randomNumberString = generateRandomString(10, {uppercase: false, lowercase: false });
+console.log(randomNumberString);
+
+//special characters only string.  
+const randomSpecialString = generateRandomString(10, {uppercase: false, lowercase: false, numbers: false, specialChars: true });
+console.log(randomSpecialString);
+
+//Password string.
+const randomPassword = generateRandomString(14, {uppercase: true, lowercase: true, numbers: true, specialChars: true });
