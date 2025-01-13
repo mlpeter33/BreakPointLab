4f48321ec880882d3eaa5aac8b0bdb798402dc07
MarianoCSC <123034983+mlpeter33@users.noreply.github.com>
2025-01-13 15:59:33 -0600

    Making strings importable

--- utils/Custom/strings.ts
+++ utils/Custom/strings.ts
@@ -1,24 +1,24 @@
 import { generateRandomString } from "../Generators/string-generator";
 
 //default behavior: alphanumeric string.
-const randomString = generateRandomString(10, {lowercase: true, uppercase: true, numbers: true });
+export const randomString = generateRandomString(10, {lowercase: true, uppercase: true, numbers: true });
 console.log(randomString);
 
 //uppercase alphanumeric string.
-const randomCapsString = generateRandomString(10, {uppercase: true, lowercase: false, numbers: false });
+export const randomCapsString = generateRandomString(10, {uppercase: true, lowercase: false, numbers: false });
 console.log(randomCapsString);
 
 //lowercase alphanumeric string.
-const randomLowerString = generateRandomString(10, {lowercase: true, uppercase: false, numbers: false });
+export const randomLowerString = generateRandomString(10, {lowercase: true, uppercase: false, numbers: false });
 console.log(randomLowerString);
 
 //numeric string.
-const randomNumberString = generateRandomString(10, {numbers: true, uppercase: false, lowercase: false });
+export const randomNumberString = generateRandomString(10, {numbers: true, uppercase: false, lowercase: false });
 console.log(randomNumberString);
 
 //special characters only string.  
-const randomSpecialString = generateRandomString(10, {uppercase: false, lowercase: false, numbers: false, specialChars: true });
+export const randomSpecialString = generateRandomString(10, {uppercase: false, lowercase: false, numbers: false, specialChars: true });
 console.log(randomSpecialString);
 
 //Password string.
-const randomPassword = generateRandomString(14, {uppercase: true, lowercase: true, numbers: true, specialChars: true });
+export const randomPassword = generateRandomString(14, {uppercase: true, lowercase: true, numbers: true, specialChars: true });
