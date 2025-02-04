8ba47549e416f32125c39f869ab67d09e3fae3fc
mlpeter33 <123034983+mlpeter33@users.noreply.github.com>
2025-02-04 07:53:55 -0600

    Update retry-wrapper.ts

--- utils/API/retry-wrapper.ts
+++ utils/API/retry-wrapper.ts
@@ -1,26 +1,28 @@
-import axios from "axios";
-import type { AxiosRequestConfig, AxiosResponse } from "axios/index";
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios/index';
 
-//Automatically retries failed API requests using exponential backoff
-
-export async function requestWithRetry<T>(
-    config: Omit<AxiosRequestConfig, "url"> & { url: string },
-    retries = 3,
-    delay = 500
+// Automatically retries failed API requests using exponential backoff
+export async function retryWrapper<T>(
+    url: string,
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+    config: AxiosRequestConfig,
+    retries: number = 3,
+    delay: number = 500
 ): Promise<AxiosResponse<T>> {
     let attempt = 0;
 
-    while (attempt < retries){
+    while (attempt < retries) {
         try {
-            return await axios(config as AxiosRequestConfig);
+            return await axios.request<T>({ ...config, url, method });
         } catch (error) {
             if (attempt < retries - 1) {
-                await new Promise((resolve) => setTimeout(resolve, delay * Math.pow(2,attempt)));
+                await new Promise((resolve) =>
+                    setTimeout(resolve, delay * Math.pow(2, attempt))
+                );
             } else {
                 throw error;
             }
         }
         attempt++;
     }
-    throw new Error('Unexpected error in requestWithRetry');
+    throw new Error("Failed to fetch data");
 }
